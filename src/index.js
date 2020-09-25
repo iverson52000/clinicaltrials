c5697152d94846f74849a62af07a114a77a90280
Albert Hsu <iverson52000@gmail.com>
2020-09-25 09:49:17 -0700

    going to install npm install @apollo/client

--- src/index.js
+++ src/index.js
@@ -5,12 +5,63 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { ApolloProvider } from 'react-apollo';
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloClient, gql } from 'apollo-boost';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: 'https://api.donotpay.com/'
+});
+
+
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache
+});
+
+client.query({
+	query: gql `
+	  	{
+		  me {
+		    _id
+		    userClinicalTrialQuery {
+		      age
+		    	genderBased
+		      age
+		      state
+		      zipCode
+		    }
+		    clinicalTrialsConnection {
+		      nodes {
+		        _id
+		        nctId
+		        contacted
+		        bookmarked
+		        clinicalTrialData {
+		          nctId
+		          studyName
+		        }
+		      }
+		    }
+		  }
+		}
+	`
+}).then((resp) => console.log(resp))
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<ApolloProvider client = {client}>
+	  <React.StrictMode>
+	    <App />
+	  </React.StrictMode>
+	</ApolloProvider>,
+	document.getElementById('root')
+
 );
 
 // If you want your app to work offline and load faster, you can change
