e643fc955f456691b2ae154fb3c374f0fdec6116
Albert Hsu <iverson52000@gmail.com>
2020-09-25 10:04:10 -0700

    successifully connect to graphql end point

--- src/index.js
+++ src/index.js
@@ -12,16 +12,25 @@ import { ApolloClient, gql } from 'apollo-boost';
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-  uri: 'https://api.donotpay.com/'
+  uri: 'https://api.donotpay.com/graphql'
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOiIyMDIxLTA5LTI0VDIwOjUyOjM2LjAxNzA4MTUxN1oiLCJpZCI6IjVmNmQwNjU1ZWZmYjFmNTM2ZTZkZDIzNiIsInBob25lIjoiIiwidXNlciI6eyJfaWQiOiI1ZjZkMDY1NWVmZmIxZjUzNmU2ZGQyMzYiLCJyb2xlcyI6W10sImVtYWlsQWRkcmVzcyI6Iml2ZXJzb241MjAwMEBnbWFpbC5jb20iLCJwYXNzd29yZCI6IiQyYSQxMCRFd0ZoQXozenJhZG9ya1kxdU83c3d1YmxmelpGTE0uVDR3Q3RCVFBtUWxEQzNaUEV4MFlHVyIsImNyZWF0ZWRBdCI6IjIwMjAtMDktMjRUMjA6NDk6MjUuMzgxWiIsInVwZGF0ZWRBdCI6IjIwMjAtMDktMjRUMjA6NDk6MjUuMzgxWiIsImJsb2NrZWQiOmZhbHNlLCJwaG9uZU51bWJlciI6IiIsInNlY3JldCI6IkZGNzJZNUtHRFJGU1REMlMiLCJwdXNoTm90aWZpY2F0aW9uVG9rZW4iOiIiLCJwbGFpZFNldHVwIjpmYWxzZSwidGFncyI6bnVsbCwic3Vic2NyaXB0aW9uQWdyZWVtZW50Q29uZmlybWF0aW9uIjpmYWxzZSwic3Vic2NyaXB0aW9uQWdyZWVtZW50Q29uZmlybWF0aW9uRGF0ZSI6bnVsbCwieWVhcmx5Q29uZmlybWF0aW9uIjpudWxsLCJjYXJkRmlyc3QiOmZhbHNlLCJjYXJkV2FpdGxpc3RKb2luZWQiOmZhbHNlLCJzdHJpcGVDdXN0b21lcklkIjpudWxsLCJzcGxpdFN1YnNjcmlwdGlvblVzZXJuYW1lIjoiIiwiY3VycmVuY3kiOiIiLCJpc0Vucm9sbGVkSW5BdXRvUHJvdGVjdCI6ZmFsc2UsImRlZmF1bHRQYXltZW50U291cmNlIjpudWxsLCJjYXJyaWVyTmFtZSI6IiIsImZvbGxvd1VwVGV4dCI6IiIsImNhcmRWaXAiOmZhbHNlLCJjdGEiOiIiLCJzaWduYXR1cmVMaW5rIjoiIiwic3Vic2NyaXB0aW9uQ2FuY2VsYWJsZSI6ZmFsc2UsInN1YnNjcmlwdGlvblVuY2FuY2VsYWJsZVJlYXNvbiI6bnVsbCwic3Vic2NyaXB0aW9uQ2FuY2VsYWJsZUF0IjpudWxsLCJzdWJzY3JpcHRpb25DYW5jZWxlZEF0IjpudWxsLCJwYXltZW50RXhlbXB0IjpmYWxzZSwid1NldHRpbmdzIjpmYWxzZSwid1VSTCI6IiIsImlPU01lbnVGZWF0dXJlcyI6bnVsbH19.3ykg5Ief254z0JIvgF-hXRqJbJOFQX2zHZ-36QQlChk"
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+	  authorization: token ? `Bearer ${token}` : "",    
+	}
+  }
 });
 
 
-
-const cache = new InMemoryCache();
-
 const client = new ApolloClient({
-  link: httpLink,
-  cache
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
 });
 
 client.query({
