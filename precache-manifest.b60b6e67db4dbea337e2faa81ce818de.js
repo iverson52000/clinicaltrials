bd6c01fda43ec5faa6cf115a02e4d538cdf97f19
Albert Hsu <iverson52000@gmail.com>
2020-09-25 17:22:37 -0700

    Updates

---
+++ precache-manifest.b60b6e67db4dbea337e2faa81ce818de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43afa7f8396381e2c24847714b84ab98",
+    "url": "/clinicaltrials/index.html"
+  },
+  {
+    "revision": "0a2334e182d7971f43c8",
+    "url": "/clinicaltrials/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "6d8958e902997b895f82",
+    "url": "/clinicaltrials/static/css/main.37f801e1.chunk.css"
+  },
+  {
+    "revision": "0a2334e182d7971f43c8",
+    "url": "/clinicaltrials/static/js/2.01ee8009.chunk.js"
+  },
+  {
+    "revision": "a73511fdd0e79fea52c5f42ad1574633",
+    "url": "/clinicaltrials/static/js/2.01ee8009.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d8958e902997b895f82",
+    "url": "/clinicaltrials/static/js/main.62ed8053.chunk.js"
+  },
+  {
+    "revision": "5e8425aa18b8b6d6e524",
+    "url": "/clinicaltrials/static/js/runtime-main.44ffd30d.js"
+  }
+]);
